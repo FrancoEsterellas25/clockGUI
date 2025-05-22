32c68bf7b485a99dce0cc508f3814edb936349ca
FrancoEsterellas25 <francoesterellas0@gmail.com>
2025-05-21 22:28:41 -0300

    Update clock.py

--- clock.py
+++ clock.py
@@ -2,6 +2,49 @@ import datetime as dt
 import tkinter as tk
 import locale
 
+
+def update_time():
+    tag2.config(text= getTime())
+    tagdate.config(text=getDay())
+    window.after(1000, update_time)
+
+my_schedule = {
+    'lunes' : [('9:00','13:00','Programación')],
+    'martes' : [('9:00','12:00','Matemática Machine Learning')],
+    'miércoles' : [('9:00','12:00','Intro. Ciencias Biomédicas'),('14:00','16:00','Filosofía')],
+    'jueves' : [('9:00','12:00','Matemática Machine Learning')],
+    'viernes' : [('9:00','12:00','taller de Internet')]
+}
+
+def isinclass():
+    now= dt.datetime.today()
+    day= now.strftime("%A")
+    hour= now.strftime("%H:%M")
+
+    if day in my_schedule:
+        for start_class, end_class, class_name in my_schedule[day]:
+            if hour > start_class and hour < end_class:
+                return True, class_name
+
+    return False, ""
+        
+def showyellow():
+    in_class, subject = isinclass()
+    if in_class:
+        window.geometry("600x138")
+        tag2.config(bg="#d9af2f",fg="#E4D593")
+        tagdate.config(bg="#d9af2f",fg="#E4D593")
+        tagclass.config(text=f"En clase {subject}...",bg="#d9af2f",fg="#E4D593")
+    else:
+        tag2.config(bg="#022001",fg="#356433")
+        tagdate.config(bg="#022001",fg="#356433")
+        tagclass.pack_forget()
+        window.geometry("600x110")
+
+    window.after(5000, showyellow)
+
+
+
 def getTime():
     now= dt.datetime.today().strftime("%H:%M:%S")
     return now
@@ -13,7 +56,7 @@ def getDay():
 
 window= tk.Tk()
 
-window.geometry("600x110")
+window.geometry("600x138")
 
 window.title('Clock 4 Focus')
 
@@ -22,13 +65,10 @@ tag2.pack(fill=tk.X)
 tagdate= tk.Label(window,bg="#022001",fg="#3C673A", font="Helvetica 25")
 tagdate.pack(fill=tk.X)
 
-
-def update_time():
-    tag2.config(text= getTime())
-    tagdate.config(text=getDay())
-    window.after(1000, update_time)
+tagclass= tk.Label(window, text="", fg="#022001", font=("Helvetica",15))
+tagclass.pack(fill=tk.X)
 
 update_time()
-
+showyellow()
 
 window.mainloop()
